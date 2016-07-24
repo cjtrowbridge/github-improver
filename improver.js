eaa260a5b74f60eebdb35a2700873f4924a5385e
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2016-07-24 09:49:26 -0700

    Update improver.js

--- improver.js
+++ improver.js
@@ -2,6 +2,12 @@ var Containers = document.getElementsByClassName("container new-discussion-timel
 
 for(var i=0, len=Containers.length; i<len; i++)
 {
-  Containers[i].style["width"] = 'calc(100% - 2em)';
-  Containers[i].style["margin"] = '0 1em 0 1em';
+  
+  if(
+    (window.location.href.indexOf("/edit/") > -1)||
+    (window.location.href.indexOf("/new/") > -1)
+  ){
+    Containers[i].style["width"] = 'calc(100% - 2em)';
+    Containers[i].style["margin"] = '0 1em 0 1em';
+  }
 }
